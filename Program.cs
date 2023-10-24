f21e62591b1fc529a9b858e5cb3a3a4ddf9de826
Kkontra <kontranowski@outlook.com>
2023-10-24 16:14:05 -0500

    SearchLibrary

--- Program.cs
+++ Program.cs
@@ -11,64 +11,75 @@ string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
 logger.Info(scrubbedFile);
 MovieFile movieFile = new MovieFile(scrubbedFile);
 logger.Info("Program ended");
+string resp;
 do
 {
     Console.WriteLine("Media Library");
     Console.WriteLine("1) View all movies.");
     Console.WriteLine("2) Add to the movie file.");
+    Console.WriteLine("3) Search movies.");
     Console.WriteLine("Enter anything else to quit.");
 
-    string resp = Console.ReadLine();
+    resp = Console.ReadLine();
     //view movies
     if (resp == "1")
-{
-    var sortedMovies = movieFile.Movies.OrderBy(m => m.title);
-    foreach(Movie m in sortedMovies)
     {
-        Console.WriteLine(m.Display());
-    }
-}
-
-else if (resp == "2")
-{
-    try
-    {   
-        Movie m = new Movie();
-        Console.WriteLine("Enter title: ");
-        m.title = Console.ReadLine();
-        Console.WriteLine("Enter all the genres and when you are finished type 'done' and press enter.");
-        string genre;
-        while((genre = Console.ReadLine()) != "done")
+        var sortedMovies = movieFile.Movies.OrderBy(m => m.title);
+        foreach (Movie m in sortedMovies)
         {
-            m.genres.Add(genre);
+            Console.WriteLine(m.Display());
         }
-                   
-        Console.WriteLine("Enter director: ");
-        m.director = Console.ReadLine();
-        Console.WriteLine("Enter runtime (h:m:s): ");
-        string runtimeInput = Console.ReadLine();
-        m.runningTime = TimeSpan.Parse(runtimeInput);   
-        
-        // Add movie to MovieFile
-        movieFile.Movies.Add(m);
-        
-        // Write all movies to file
-        using (StreamWriter sw = new StreamWriter(movieFile.filePath))
+    }
+    
+    else if (resp == "2")
+    {
+        try
         {
-            foreach (var movie in movieFile.Movies)
+            Movie m = new Movie();
+            Console.WriteLine("Enter title: ");
+            m.title = Console.ReadLine();
+            Console.WriteLine("Enter all the genres and when you are finished type 'done' and press enter.");
+            string genre;
+            while ((genre = Console.ReadLine()) != "done")
             {
-                sw.WriteLine($"{movie.title},{string.Join("|", movie.genres)},{movie.director},{movie.runningTime}");
+                m.genres.Add(genre);
+            }
+
+            Console.WriteLine("Enter director: ");
+            m.director = Console.ReadLine();
+            Console.WriteLine("Enter runtime (h:m:s): ");
+            string runtimeInput = Console.ReadLine();
+            m.runningTime = TimeSpan.Parse(runtimeInput);
+
+            // Add movie to MovieFile
+            movieFile.Movies.Add(m);
+
+            // Write all movies to file
+            using (StreamWriter sw = new StreamWriter(movieFile.filePath))
+            {
+                foreach (var movie in movieFile.Movies)
+                {
+                    sw.WriteLine($"{movie.title},{string.Join("|", movie.genres)},{movie.director},{movie.runningTime}");
+                }
             }
         }
+        catch (IOException e)
+        {
+            logger.Error(e, "An error occured entering in the information.");
+        }
     }
-    catch (IOException e)
+    else if (resp == "3")
     {
-        logger.Error(e, "An error occured entering in the information.");    
-    }      
-}
+            Console.WriteLine("Enter title: ");
+            string choosenTitle = Console.ReadLine();
+            var titlesReturned = movieFile.Movies.Where(m => m.title.Contains(choosenTitle, StringComparison.CurrentCultureIgnoreCase)).Select(m => m.title);
 
-} while (true);
-{
+            foreach (string t in titlesReturned)
+            {
+                Console.WriteLine($"Title:  {t}");
+                
+            }
+              Console.WriteLine($"There are {titlesReturned.Count()} movies with \"{choosenTitle}\" in the title:");
+        }
 
-}
-
+} while (resp == "1" || resp == "2" || resp == "3");
